8c5dc13f62ad10bc3e26007d2f9418589a0995d4
Gulle4559 <next16069@edu.nextkbh.dk>
2025-02-26 09:44:23 +0100

    Start

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function preload(){
+bil1=("Bil1.jpg")
+bil2=("Bil2.jpg")
+bil3=("Bil3..jpg")
+bil4=("Bil4.jpg")
+bane=("Bane1.jpg")
+}
+
+
+function draw(){
+Canvas(200.200)
+loadImage("Bane1.jpg")
+
+
+
+}
