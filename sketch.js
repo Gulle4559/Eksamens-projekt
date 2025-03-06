03fafe427c5d27221c1792af5d74278de3ce7faf
Gulle4559 <next16069@edu.nextkbh.dk>
2025-03-06 14:49:21 +0100

    bil2 virker

--- sketch.js
+++ sketch.js
@@ -1,13 +1,11 @@
-
 let playerHeight = 50;
 let playerWidth = 50;
 let playerX = 50;
 let playerY = 50;
-
+let bil2X = 50;
+let bil2Y =110;
 
 let bane;
-
-
 
 function preload(){
 bil1= loadImage("Bil1.png");
@@ -22,22 +20,12 @@ bane = loadImage("Bane1.jpg");
 
 function setup(){
 createCanvas(600,600);
+background(250);
+bane.resize(400,400);
+image(bane, 0, 0); 
 
+image(bil1,playerX,playerY,playerWidth,playerHeight);
 
-
+image(bil2,bil2X,bil2Y,playerWidth,playerHeight)
 
 }
-
-
-function draw(){
-
-    background(250);
-    bane.resize(400,400);
-    image(bane, 0, 0); 
-    
-    image(bil1,playerX,playerY,playerWidth,playerHeight);
-}
-
-
-
-
