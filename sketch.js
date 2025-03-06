5a538ff1d2eb62f9dfd4c637d31b7798a421d38f
Gulle4559 <next16069@edu.nextkbh.dk>
2025-03-06 14:33:50 +0100

    bagrund og spiller 1 tegnet ind

--- sketch.js
+++ sketch.js
@@ -1,16 +1,43 @@
+
+let playerHeight = 50;
+let playerWidth = 50;
+let playerX = 50;
+let playerY = 50;
+
+
+let bane;
+
+
+
 function preload(){
-bil1=("Bil1.jpg")
-bil2=("Bil2.jpg")
-bil3=("Bil3..jpg")
-bil4=("Bil4.jpg")
-bane=("Bane1.jpg")
+bil1= loadImage("Bil1.png");
+bil2= loadImage("Bil2.png");
+bil3= loadImage("Bil3.png");
+bil4= loadImage("Bil4.png");
+
+bane = loadImage("Bane1.jpg");
+
+}
+
+
+function setup(){
+createCanvas(600,600);
+
+
+
+
 }
 
 
 function draw(){
-Canvas(200.200)
-loadImage("Bane1.jpg")
 
-
-
+    background(250);
+    bane.resize(400,400);
+    image(bane, 0, 0); 
+    
+    image(bil1,playerX,playerY,playerWidth,playerHeight);
 }
+
+
+
+
