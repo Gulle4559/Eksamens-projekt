e28dc966674d15f41cce95884558585ecb78ce24
Gulle4559 <next16069@edu.nextkbh.dk>
2025-03-20 13:15:05 +0100

    bil 1 kan nu rykke frem

--- sketch.js
+++ sketch.js
@@ -2,14 +2,21 @@ let playerHeight = 90;
 let playerWidth = 90;
 let playerX = 50;
 let playerY = 140;
+
 let bil2X = 50;
 let bil2Y = 260;
+
 let bil3X = 50;
 let bil3Y = 380;
+
 let bil4X = 50;
 let bil4Y = 500;
 
-
+let image1
+let image2
+let image3
+let image4
+let imageM
 
 let målstreg;
 let målstregX = 700;
@@ -21,14 +28,17 @@ let bane;
 
 
 
-function preload(){
-bil1= loadImage("Bil1.png");
-bil2= loadImage("Bil2.png");
-bil3= loadImage("Bil3.png");
-bil4= loadImage("Bil4.png");
+
+
+
+function preload(){// loader alle billeder 
+image1 = loadImage("Bil1.png");
+image2 = loadImage("Bil2.png");
+image3 = loadImage("Bil3.png");
+image4 = loadImage("Bil4.png");
 
 bane = loadImage("Bane1.jpg");
-målstreg = loadImage("målstreg1.png");
+imageM = loadImage("målstreg1.png");
 
 
 }
@@ -37,20 +47,22 @@ målstreg = loadImage("målstreg1.png");
 function setup(){
 createCanvas(800,800);
 background(250);
-bane.resize(800,800);
-image(bane, 0, 0); 
+bane.resize(800,800);// bane tilpasset
+image(bane, 0, 0); // bane lagt ind
 
-image(bil1,playerX,playerY,playerWidth,playerHeight);
+//image1(bil1,playerX,playerY,playerWidth,playerHeight);// spiller
 
-image(bil2,bil2X,bil2Y,playerWidth,playerHeight)
+//image2(bil2,bil2X,bil2Y,playerWidth,playerHeight)// bil2
 
-image(bil3,bil3X,bil3Y,playerWidth,playerHeight)
+//image3(bil3,bil3X,bil3Y,playerWidth,playerHeight)// bil3
 
-image(bil4,bil4X,bil4Y,playerWidth,playerHeight)
+//image4(bil4,bil4X,bil4Y,playerWidth,playerHeight)// bil4
 
 
 
-image(målstreg,målstregX,målstregY,målstregWidth,målstregHeight)
+//imageM(målstreg,målstregX,målstregY,målstregWidth,målstregHeight)// målestreg
+
+
 
 
 rect(250, 650, 270, 37);
@@ -61,6 +73,33 @@ text('Spørgsmål 1', 300, 680);
 rect(150, 740, 200, 30);// venstre
 
 rect(380, 740, 200, 30);//højre
+
 }
 
+function mousePressed() {
+    // Hvis klik er indenfor højre boks
+    if (mouseX >= 380 && mouseX <= 580 && mouseY >= 740 && mouseY <= 770) {
+        playerX +=10 
 
+        
+
+
+
+}
+
+}
+
+function draw(){
+    
+    image(image1,playerX,playerY,playerHeight,playerWidth)
+    
+    image(image2,bil2X,bil2Y,playerHeight,playerWidth)
+    
+    image(image3,bil3X,bil3Y,playerHeight,playerWidth)
+    
+    image(image4,bil4X,bil4Y,playerHeight,playerWidth)
+    
+    image(imageM,målstregX,målstregY,målstregWidth,målstregHeight)
+    
+
+    }
